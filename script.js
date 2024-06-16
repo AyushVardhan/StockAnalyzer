7c097ec5fa7c1f0d615ee92b7f38eb32d6b2a764
Ayush Vardhan <ayushvardhan@Ayushs-MacBook-Pro.local>
2024-06-16 16:43:50 +0530

    add etension

---
+++ script.js
@@ -0,0 +1,8 @@
+async function fetchData() {
+    // const res=await fetch ("https://api.coronavirus.data.gov.uk/v1/data");
+    // const record=await res.json();
+    document.getElementById("stockName").innerHTML="Reliance Industries"
+    document.getElementById("analysis").innerHTML="Dummy data, Dummy data, Dummy data, Dummy data, Dummy data, Dummy data, Dummy data, Dummy data, Dummy data, Dummy data, Dummy data, Dummy data, Dummy data, Dummy data, Dummy data";
+    // document.getElementById("analysis").innerHTML="record.data[0].areaName";
+}
+fetchData();
